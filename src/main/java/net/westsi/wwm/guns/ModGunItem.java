7fb17d9c6e3449ccbcc87c4b507a5735c0fd5609
Westsi <jamie@rockyroost.com>
2022-02-01 17:10:24 +0000

    g

--- src/main/java/net/westsi/wwm/guns/ModGunItem.java
+++ src/main/java/net/westsi/wwm/guns/ModGunItem.java
@@ -1,4 +1,21 @@
 package net.westsi.wwm.guns;
 
-public class ModGunItem {
+import net.minecraft.item.BowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.UseAction;
+
+public class ModGunItem extends BowItem {
+    public ModGunItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack) {
+        return 20;
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.CROSSBOW;
+    }
 }
---
+++ src/main/java/net/westsi/wwm/items/ModItems.java
@@ -0,0 +1,26 @@
+package net.westsi.wwm.items;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.westsi.wwm.WWM;
+import net.westsi.wwm.guns.ModGunItem;
+
+public class ModItems {
+
+
+    public static final Item PISTOL = registerItem("pistol",
+            new ModGunItem(new FabricItemSettings().fireproof().group(ItemGroup.MISC)));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(WWM.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        System.out.println("Registering Mod Items for " + WWM.MOD_ID);
+    }
+
+}
